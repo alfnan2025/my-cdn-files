202e882d2dcc6b8cbcea6a8c09278af2f12215cb
alfnan2025 <review.r5es@gmail.com>
2025-03-07 16:29:22 +0200

    Update tqem.js

--- tqem.js
+++ tqem.js
@@ -1,3 +1,192 @@
 <script>
-const _0x107328=_0x5aba;(function(_0x556e8d,_0x418741){const _0x1c1a80=_0x5aba,_0x2584d5=_0x556e8d();while(!![]){try{const _0x1ae31f=parseInt(_0x1c1a80(0x88))/0x1*(-parseInt(_0x1c1a80(0xae))/0x2)+-parseInt(_0x1c1a80(0xb2))/0x3+-parseInt(_0x1c1a80(0x93))/0x4+-parseInt(_0x1c1a80(0xd0))/0x5+parseInt(_0x1c1a80(0xa9))/0x6*(-parseInt(_0x1c1a80(0xdb))/0x7)+-parseInt(_0x1c1a80(0xc4))/0x8+parseInt(_0x1c1a80(0xe3))/0x9;if(_0x1ae31f===_0x418741)break;else _0x2584d5['push'](_0x2584d5['shift']());}catch(_0x180471){_0x2584d5['push'](_0x2584d5['shift']());}}}(_0x413c,0x1f8d7),document['addEventListener'](_0x107328(0xa2),function(){const _0x29a100=_0x107328;let _0x3ed330=0x0;const _0x1a3219=_0x29a100(0x7e),_0xa20a7d=window[_0x29a100(0xd1)][_0x29a100(0xa0)][_0x29a100(0xd9)]('/')[_0x29a100(0x7d)](),_0x12e77a=_0x29a100(0xd3)+_0xa20a7d;_0x3d510c();if(localStorage['getItem'](_0x12e77a)){document[_0x29a100(0xe4)](_0x29a100(0xa6))['innerHTML']='<p\x20style=\x27color:\x20red;\x27>لقد\x20قمت\x20بإرسال\x20تقييم\x20لهذا\x20المنتج.</p>';return;}document[_0x29a100(0x75)]('.star')[_0x29a100(0x96)](_0x207aae=>{const _0x570378=_0x29a100;_0x207aae[_0x570378(0x83)](_0x570378(0xb7),function(){const _0x2d78d7=_0x570378;_0x3ed330=parseInt(this[_0x2d78d7(0x98)][_0x2d78d7(0xcc)]),document[_0x2d78d7(0x75)]('.star')['forEach'](_0x1fbccf=>_0x1fbccf[_0x2d78d7(0xd6)][_0x2d78d7(0xc0)]=_0x1fbccf['dataset']['value']<=_0x3ed330?_0x2d78d7(0xd7):_0x2d78d7(0xba));});});function _0x1a60ef(_0x36cda0){const _0x54e019=_0x29a100;return/^[\u0600-\u06FF\s]+$/[_0x54e019(0xa3)](_0x36cda0);}document[_0x29a100(0xe4)]('username')[_0x29a100(0x83)](_0x29a100(0xa7),function(){const _0x23aafd=_0x29a100;!_0x1a60ef(this['value'])&&(this[_0x23aafd(0xcc)]=this[_0x23aafd(0xcc)][_0x23aafd(0xaa)](/[^\u0600-\u06FF\s]/g,''));}),document[_0x29a100(0xe4)](_0x29a100(0xda))[_0x29a100(0x83)](_0x29a100(0xa7),function(){const _0x13cf0e=_0x29a100;!_0x1a60ef(this[_0x13cf0e(0xcc)])&&(this[_0x13cf0e(0xcc)]=this[_0x13cf0e(0xcc)]['replace'](/[^\u0600-\u06FF\s]/g,''));}),document['getElementById'](_0x29a100(0x99))[_0x29a100(0x83)]('click',function(_0x3acdc6){const _0x1fa870=_0x29a100;_0x3acdc6[_0x1fa870(0x86)]();let _0x476272=document['getElementById'](_0x1fa870(0xde))[_0x1fa870(0xcc)]['trim']()||_0x1fa870(0xb0),_0x3b6f25=document[_0x1fa870(0xe4)](_0x1fa870(0xda))[_0x1fa870(0xcc)][_0x1fa870(0xc7)]();if(_0x3ed330===0x0||_0x3b6f25===''){alert(_0x1fa870(0x8e));return;}if(!_0x1a60ef(_0x476272)||!_0x1a60ef(_0x3b6f25)){alert(_0x1fa870(0xe1));return;}let _0x3e0658=new URLSearchParams();_0x3e0658['append'](_0x1fa870(0xcd),_0xa20a7d),_0x3e0658[_0x1fa870(0xd4)]('username',_0x476272),_0x3e0658['append'](_0x1fa870(0xa4),_0x3ed330),_0x3e0658['append'](_0x1fa870(0xda),_0x3b6f25),_0x39c58c(_0x3e0658);});function _0x39c58c(_0x1a993f){const _0x19d8cb=_0x29a100;fetch(_0x1a3219,{'method':_0x19d8cb(0xac),'headers':{'Content-Type':_0x19d8cb(0x87)},'body':_0x1a993f['toString']()})['then'](_0x2c456e=>_0x2c456e['json']())[_0x19d8cb(0xa5)](_0x3e71a3=>{const _0x2da078=_0x19d8cb;alert(_0x3e71a3[_0x2da078(0x78)]),_0x3e71a3[_0x2da078(0xcb)]===_0x2da078(0xbd)&&(localStorage[_0x2da078(0xbf)](_0x12e77a,_0x2da078(0x80)),document[_0x2da078(0xe4)](_0x2da078(0xa6))[_0x2da078(0x94)]=_0x2da078(0x82),_0x3d510c());})[_0x19d8cb(0x92)](_0x15883=>{const _0x1af499=_0x19d8cb;console[_0x1af499(0xad)](_0x1af499(0x7a),_0x15883),alert(_0x1af499(0xc1));});}async function _0x3d510c(){const _0x295078=_0x29a100;try{if(localStorage[_0x295078(0x9c)]('reviews_'+_0xa20a7d)){let _0x16f127=JSON['parse'](localStorage['getItem']('reviews_'+_0xa20a7d));_0x2542a1(_0x16f127);}let _0x2b152a=await fetch(_0x1a3219+'?postId='+_0xa20a7d);if(!_0x2b152a['ok'])throw new Error(_0x295078(0x77));let _0x19c4cb=await _0x2b152a['json']();localStorage['setItem']('reviews_'+_0xa20a7d,JSON[_0x295078(0xaf)](_0x19c4cb)),_0x2542a1(_0x19c4cb);}catch(_0xb83f22){console[_0x295078(0xad)](_0x295078(0x8d),_0xb83f22),document[_0x295078(0xe4)](_0x295078(0x8a))[_0x295078(0x94)]=_0x295078(0x89);}}function _0x2542a1(_0x8de0a4){const _0x42c831=_0x29a100;let _0x6fe355=document[_0x42c831(0xe4)](_0x42c831(0x8a));_0x6fe355['innerHTML']=_0x42c831(0x7b);if(_0x8de0a4[_0x42c831(0xa1)]===0x0)_0x6fe355[_0x42c831(0x94)]+=_0x42c831(0xdd);else{let _0x2be40e=0x0;_0x8de0a4[_0x42c831(0x96)](_0x2e781c=>{_0x2be40e+=parseInt(_0x2e781c['stars']);});let _0x61055e=(_0x2be40e/_0x8de0a4[_0x42c831(0xa1)])[_0x42c831(0xb3)](0x2),_0x13abb1=_0xd078ef(_0x61055e);_0x6fe355['innerHTML']+=_0x42c831(0xbb)+_0x13abb1+_0x42c831(0x8c);const _0x11487b=_0x8de0a4[_0x42c831(0x7c)](0x0,0x3);_0x11487b[_0x42c831(0x96)](_0x146f87=>{const _0x1bf5f4=_0x42c831;let _0x1f79e2='★'['repeat'](parseInt(_0x146f87[_0x1bf5f4(0xa4)]))+'☆'[_0x1bf5f4(0xb8)](0x5-parseInt(_0x146f87[_0x1bf5f4(0xa4)]));_0x6fe355[_0x1bf5f4(0x94)]+='\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22review\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<strong>'+_0x146f87['username']+_0x1bf5f4(0xb9)+_0x1f79e2+'\x20('+_0x146f87[_0x1bf5f4(0xa4)]+_0x1bf5f4(0x76)+_0x146f87[_0x1bf5f4(0xda)]+_0x1bf5f4(0xe2);});const _0x398c24=_0x8de0a4[_0x42c831(0x7c)](0x3);_0x398c24[_0x42c831(0x96)](_0x3aa758=>{const _0xba3826=_0x42c831;let _0x227b39='★'[_0xba3826(0xb8)](parseInt(_0x3aa758[_0xba3826(0xa4)]))+'☆'[_0xba3826(0xb8)](0x5-parseInt(_0x3aa758[_0xba3826(0xa4)]));_0x6fe355[_0xba3826(0x94)]+=_0xba3826(0xcf)+_0x3aa758['username']+_0xba3826(0xb9)+_0x227b39+'\x20('+_0x3aa758[_0xba3826(0xa4)]+_0xba3826(0x76)+_0x3aa758[_0xba3826(0xda)]+_0xba3826(0xe2);}),_0x6fe355[_0x42c831(0x83)](_0x42c831(0xc9),function(){const _0x587d1e=_0x42c831;if(_0x6fe355[_0x587d1e(0xd8)]+_0x6fe355['clientHeight']>=_0x6fe355[_0x587d1e(0xc8)]){const _0xdfc2b3=document[_0x587d1e(0x75)]('.review.hidden');_0xdfc2b3[_0x587d1e(0x96)](_0x25a083=>{const _0x1a03cc=_0x587d1e;_0x25a083[_0x1a03cc(0xd2)][_0x1a03cc(0xc3)]('hidden');});}});}}function _0xd078ef(_0x2f0c52){const _0x1d068f=_0x29a100;let _0xf10e7=Math['floor'](_0x2f0c52),_0x1d724e=_0x2f0c52-_0xf10e7>=0.5?'★':'☆',_0x1f4a4b=0x5-_0xf10e7-(_0x1d724e==='★'?0x1:0x0),_0x5ac1a6='★'[_0x1d068f(0xb8)](_0xf10e7);if(_0x1d724e==='★')_0x5ac1a6+='★';return _0x5ac1a6+='☆'[_0x1d068f(0xb8)](_0x1f4a4b),_0x5ac1a6;}}));var _0x173fb8=_0x2d59;function _0x40a8(){const _0x550cdb=_0x107328;var _0x1c4e74=[_0x550cdb(0xdf),'12NVtPrs',_0x550cdb(0xdc),_0x550cdb(0xd4),'json','href','webcache.googleusercontent',_0x550cdb(0x81),_0x550cdb(0xc2),'post-preview','style',_0x550cdb(0xa8),_0x550cdb(0x84),_0x550cdb(0xc5),_0x550cdb(0xbe),_0x550cdb(0xb4),_0x550cdb(0x9f),_0x550cdb(0x9a),'158308QewGdJ',_0x550cdb(0xb5),_0x550cdb(0x8b),_0x550cdb(0x9d),_0x550cdb(0xce),_0x550cdb(0xbc),_0x550cdb(0xca),_0x550cdb(0x9b),_0x550cdb(0xb6),_0x550cdb(0x79),_0x550cdb(0x85),_0x550cdb(0xa1),_0x550cdb(0x91),_0x550cdb(0x9e),'.redirect',_0x550cdb(0x90),_0x550cdb(0xb1),_0x550cdb(0xab),_0x550cdb(0xe0),_0x550cdb(0x95),'hasClass',_0x550cdb(0xc6),_0x550cdb(0xd1),_0x550cdb(0x8f)];return _0x40a8=function(){return _0x1c4e74;},_0x40a8();}function _0x2d59(_0x13eac8,_0x573a4f){var _0x1a1874=_0x40a8();return _0x2d59=function(_0x728f1,_0x3a62eb){_0x728f1=_0x728f1-0xb0;var _0x283ec1=_0x1a1874[_0x728f1];return _0x283ec1;},_0x2d59(_0x13eac8,_0x573a4f);}function _0x5aba(_0x3497b9,_0x56a75d){const _0x413c23=_0x413c();return _0x5aba=function(_0x5abaa3,_0x55034f){_0x5abaa3=_0x5abaa3-0x75;let _0x28c34f=_0x413c23[_0x5abaa3];return _0x28c34f;},_0x5aba(_0x3497b9,_0x56a75d);}function _0x413c(){const _0x571043=['2LDRMxn','stringify','مجهول','find','331638WZOcMt','toFixed','8297016XIrDDZ','jsonp','content','click','repeat','</strong>\x20-\x20','gray','<p><strong>التقييم\x20العام:\x20','12249mvWGae','success','push','setItem','color','حدث\x20خطأ\x20أثناء\x20إرسال\x20التقييم.','GET','remove','313584xOplcx','b/layout-preview','ready','trim','scrollHeight','scroll','html','status','value','postId','toLowerCase','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22review\x20hidden\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<strong>','333340XknMeu','location','classList','reviewed_','append','shift','style','gold','scrollTop','split','comment','7zpSlVD','entry','<p>لا\x20توجد\x20تقييمات\x20لهذا\x20المنتج\x20بعد.</p>','username','5134314aiKElJ','https://www.blogger.com/feeds/','يرجى\x20إدخال\x20النص\x20باللغة\x20العربية\x20فقط.','</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20','6520167VWQyYf','getElementById','querySelectorAll','/5)\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p>','فشل\x20تحميل\x20التقييمات.','message','5BCiLtk','Error:','<h3>التقييمات</h3>','slice','pop','https://script.google.com/macros/s/AKfycbxAznk5rudobevlDA2NsBUHW9LxYikCm8LACmNKGTCYAmm4efCUiooNwh_xmHqKOQ3qYA/exec','error_page','true','text','<p\x20style=\x27color:\x20red;\x27>شكرًا!\x20لقد\x20قمت\x20بإرسال\x20تقييم\x20لهذا\x20المنتج.</p>','addEventListener','2430hqYDnl','template-editor','preventDefault','application/x-www-form-urlencoded','116240lcofMm','حدث\x20خطأ\x20أثناء\x20تحميل\x20التقييمات.','reviews','445287rscCSB','</strong></p>','خطأ:','يرجى\x20اختيار\x20عدد\x20النجوم\x20وكتابة\x20تعليق','feed','2TstlkJ','8666400876123099283','catch','241472cwsoZW','innerHTML','hostname','forEach','script','dataset','submit-review','11893992lNqExP','9718992vrBBMY','getItem','translate.google','indexOf','/posts/default?alt=json-in-script','pathname','length','DOMContentLoaded','test','stars','then','review-form','input','www.blogger','1213224ZaTGjV','replace','head','POST','error'];_0x413c=function(){return _0x571043;};return _0x413c();}(function(_0x1c87f1,_0x2ab567){const _0x28e2a9=_0x107328;var _0xe16bdb=_0x2d59,_0x4e3091=_0x1c87f1();while(!![]){try{var _0x397597=-parseInt(_0xe16bdb(0xb4))/0x1*(-parseInt(_0xe16bdb(0xc3))/0x2)+-parseInt(_0xe16bdb(0xb6))/0x3*(-parseInt(_0xe16bdb(0xcd))/0x4)+parseInt(_0xe16bdb(0xbd))/0x5*(-parseInt(_0xe16bdb(0xcc))/0x6)+parseInt(_0xe16bdb(0xb1))/0x7+parseInt(_0xe16bdb(0xbb))/0x8+-parseInt(_0xe16bdb(0xb9))/0x9*(parseInt(_0xe16bdb(0xd8))/0xa)+-parseInt(_0xe16bdb(0xb3))/0xb;if(_0x397597===_0x2ab567)break;else _0x4e3091[_0x28e2a9(0xbe)](_0x4e3091[_0x28e2a9(0xd5)]());}catch(_0x57caa9){_0x4e3091[_0x28e2a9(0xbe)](_0x4e3091[_0x28e2a9(0xd5)]());}}}(_0x40a8,0xb3b2b),$(document)[_0x173fb8(0xc9)](function(){var _0x303cc4=_0x173fb8,_0x2ba01e=_0x303cc4(0xc0);$(function(){'use strict';var _0x4eeb78=_0x303cc4;$['ajax']({'dataType':_0x4eeb78(0xd0),'url':_0x4eeb78(0xc6)+_0x2ba01e+_0x4eeb78(0xb2),'method':_0x4eeb78(0xd4),'dataType':_0x4eeb78(0xb5),'success':function(_0x2177de){const _0x3ddfe9=_0x5aba;var _0x1cecdf=_0x4eeb78,_0x1fd571;for(_0x1fd571=0x0;_0x1fd571<_0x2177de[_0x1cecdf(0xcb)][_0x1cecdf(0xce)][_0x3ddfe9(0xa1)];_0x1fd571+=0x1){var _0x52c946=$(_0x2177de[_0x1cecdf(0xcb)][_0x1cecdf(0xce)][_0x1fd571][_0x1cecdf(0xbc)]['$t']);if(0x0===_0x1fd571&&!$('body')[_0x1cecdf(0xc8)](_0x3ddfe9(0x7f))){for(var _0x323800=_0x52c946[_0x1cecdf(0xc4)]('li'),_0x3da731=[],_0x10afb0=0x0;_0x10afb0<_0x323800[_0x1cecdf(0xbf)];_0x10afb0+=0x1)_0x3da731[_0x1cecdf(0xb0)]($(_0x323800[_0x10afb0])[_0x1cecdf(0xd3)]());var _0x53b1a6,_0x4bd154=window[_0x1cecdf(0xca)][_0x1cecdf(0xc7)][_0x1cecdf(0xb8)](),_0x55c52e=window[_0x3ddfe9(0xd1)][_0x1cecdf(0xd1)][_0x1cecdf(0xb8)](),_0x413486=_0x3da731[_0x1cecdf(0xbf)]-0x1;for(_0x53b1a6=0x0;_0x53b1a6<_0x3da731['length'];_0x53b1a6+=0x1){if(-0x1!=_0x4bd154[_0x1cecdf(0xc1)](_0x3da731[_0x53b1a6])){var _0x48e41f=$(_0x2177de[_0x1cecdf(0xcb)][_0x1cecdf(0xce)][_0x1fd571][_0x1cecdf(0xbc)]['$t'])[_0x3ddfe9(0xb1)](_0x3ddfe9(0x97)),_0xd8cfdb=$(_0x2177de[_0x1cecdf(0xcb)][_0x1cecdf(0xce)][_0x1fd571][_0x1cecdf(0xbc)]['$t'])[_0x3ddfe9(0xb1)](_0x1cecdf(0xd6));$(_0x1cecdf(0xc5))[_0x3ddfe9(0xd4)](_0xd8cfdb),$(_0x3ddfe9(0xab))[_0x3ddfe9(0xd4)](_0x48e41f);break;}_0x53b1a6==_0x413486&&-0x1==_0x55c52e[_0x3ddfe9(0x9e)](_0x1cecdf(0xd5))&&-0x1==_0x55c52e[_0x3ddfe9(0x9e)](_0x1cecdf(0xd7))&&-0x1==_0x55c52e[_0x1cecdf(0xc1)](_0x1cecdf(0xd9))&&-0x1==_0x55c52e[_0x1cecdf(0xc1)]('b/preview')&&-0x1==_0x55c52e[_0x1cecdf(0xc1)](_0x1cecdf(0xb7))&&-0x1==_0x55c52e[_0x1cecdf(0xc1)](_0x1cecdf(0xd2))&&-0x1==_0x55c52e[_0x1cecdf(0xc1)](_0x1cecdf(0xbe))&&$(_0x3ddfe9(0xca))[_0x1cecdf(0xba)](_0x52c946[_0x1cecdf(0xc4)](_0x1cecdf(0xc2))[_0x3ddfe9(0xca)]());}}0x1===_0x1fd571&&(_0xd8cfdb=$(_0x2177de[_0x3ddfe9(0x8f)][_0x1cecdf(0xce)][_0x1fd571][_0x3ddfe9(0xb6)]['$t'])[_0x1cecdf(0xc4)](_0x1cecdf(0xd6)),$(_0x1cecdf(0xc5))[_0x1cecdf(0xcf)](_0xd8cfdb));}}});});}));
+
+  
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+  let selectedStars = 0;
+  const scriptURL = "https://script.google.com/macros/s/AKfycbxAznk5rudobevlDA2NsBUHW9LxYikCm8LACmNKGTCYAmm4efCUiooNwh_xmHqKOQ3qYA/exec";
+  const postId = window.location.pathname.split("/").pop(); // ✅ استخراج معرف التدوينة
+  const reviewKey = "reviewed_" + postId; // ✅ تخزين التقييم لكل تدوينة على حدة
+
+  // ✅ تحميل التقييمات فورًا عند تحميل الصفحة
+  loadReviews();
+
+  // ✅ التحقق مما إذا كان المستخدم قد قام بالتقييم سابقًا
+  if (localStorage.getItem(reviewKey)) {
+    document.getElementById("review-form").innerHTML = "<p style='color: red;'>لقد قمت بإرسال تقييم لهذه التدوينة.</p>";
+    return;
+  }
+
+  // ✅ تحديد عدد النجوم عند التقييم
+  document.querySelectorAll(".star").forEach(star => {
+    star.addEventListener("click", function () {
+      selectedStars = parseInt(this.dataset.value);
+      document.querySelectorAll(".star").forEach(s => 
+        s.style.color = s.dataset.value <= selectedStars ? "gold" : "gray"
+      );
+    });
+  });
+
+  // ✅ التحقق من إدخال اللغة العربية فقط
+  function isArabic(text) {
+    return /^[\u0600-\u06FF\s]+$/.test(text);
+  }
+
+  document.getElementById("username").addEventListener("input", function () {
+    if (!isArabic(this.value)) {
+      this.value = this.value.replace(/[^\u0600-\u06FF\s]/g, '');
+    }
+  });
+
+  document.getElementById("comment").addEventListener("input", function () {
+    if (!isArabic(this.value)) {
+      this.value = this.value.replace(/[^\u0600-\u06FF\s]/g, '');
+    }
+  });
+
+  // ✅ إرسال التقييم
+  document.getElementById("submit-review").addEventListener("click", function (event) {
+    event.preventDefault(); // منع إعادة تحميل الصفحة
+
+    let username = document.getElementById("username").value.trim() || "مجهول";
+    let comment = document.getElementById("comment").value.trim();
+
+    if (selectedStars === 0 || comment === "") {
+      alert("يرجى اختيار عدد النجوم وكتابة تعليق");
+      return;
+    }
+
+    if (!isArabic(username) || !isArabic(comment)) {
+      alert("يرجى إدخال النص باللغة العربية فقط.");
+      return;
+    }
+
+    // ✅ تجهيز البيانات بصيغة x-www-form-urlencoded
+    let formData = new URLSearchParams();
+    formData.append("postId", postId);
+    formData.append("username", username);
+    formData.append("stars", selectedStars);
+    formData.append("comment", comment);
+
+    // ✅ إرسال البيانات إلى جوجل جداول
+    sendReview(formData);
+  });
+
+  // ✅ إرسال التقييم إلى Google Sheets
+  function sendReview(formData) {
+    fetch(scriptURL, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: formData.toString()
+    })
+    .then(response => response.json())
+    .then(data => {
+      alert(data.message);
+      if (data.status === "success") {
+        localStorage.setItem(reviewKey, "true"); // ✅ حفظ حالة التقييم لكل تدوينة
+        document.getElementById("review-form").innerHTML = "<p style='color: red;'>شكرًا! لقد قمت بإرسال تقييم لهذه التدوينة.</p>";
+        loadReviews(); // تحديث التقييمات بعد الإرسال
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      alert("حدث خطأ أثناء إرسال التقييم.");
+    });
+  }
+
+  // ✅ تحميل التقييمات من Google Sheets حسب معرف التدوينة
+  async function loadReviews() {
+    try {
+      // ✅ التحقق إذا كانت التقييمات موجودة في الذاكرة المحلية
+      if (localStorage.getItem('reviews_' + postId)) {
+        let cachedReviews = JSON.parse(localStorage.getItem('reviews_' + postId));
+        renderReviews(cachedReviews);
+      }
+
+      // ✅ جلب التقييمات من Google Sheets فقط إذا كانت غير موجودة في الذاكرة المحلية
+      let response = await fetch(`${scriptURL}?postId=${postId}`);
+      if (!response.ok) throw new Error("فشل تحميل التقييمات.");
+      
+      let data = await response.json();
+      localStorage.setItem('reviews_' + postId, JSON.stringify(data)); // تخزين التقييمات في الذاكرة المحلية
+      renderReviews(data);
+
+    } catch (error) {
+      console.error("خطأ:", error);
+      document.getElementById("reviews").innerHTML = "حدث خطأ أثناء تحميل التقييمات.";
+    }
+  }
+
+  // ✅ عرض التقييمات
+  function renderReviews(data) {
+    let reviewsContainer = document.getElementById("reviews");
+    reviewsContainer.innerHTML = "<h3>التقييمات</h3>";
+
+    if (data.length === 0) {
+      reviewsContainer.innerHTML += "<p>لا توجد تقييمات لهذه التدوينة بعد.</p>";
+    } else {
+      // حساب التقييم العام
+      let totalStars = 0;
+      data.forEach(review => {
+        totalStars += parseInt(review.stars);
+      });
+      let averageStars = (totalStars / data.length).toFixed(2);
+
+      // عرض التقييم العام على شكل نجوم
+      let averageStarsHtml = generateStarsHtml(averageStars);
+
+      // عرض التقييم العام
+      reviewsContainer.innerHTML += `<p><strong>التقييم العام: ${averageStarsHtml}</strong></p>`;
+
+      // عرض أول 3 تقييمات فقط
+      const visibleReviews = data.slice(0, 3);
+      visibleReviews.forEach(review => {
+        let stars = "★".repeat(parseInt(review.stars)) + "☆".repeat(5 - parseInt(review.stars));
+        reviewsContainer.innerHTML += `
+          <div class="review">
+            <strong>${review.username}</strong> - ${stars} (${review.stars}/5)
+            <p>${review.comment}</p>
+          </div>
+        `;
+      });
+
+      // إضافة تقييمات مخفية مع تحميل المزيد عند التمرير
+      const hiddenReviews = data.slice(3);
+      hiddenReviews.forEach(review => {
+        let stars = "★".repeat(parseInt(review.stars)) + "☆".repeat(5 - parseInt(review.stars));
+        reviewsContainer.innerHTML += `
+          <div class="review hidden">
+            <strong>${review.username}</strong> - ${stars} (${review.stars}/5)
+            <p>${review.comment}</p>
+          </div>
+        `;
+      });
+
+      // تفعيل التمرير لإظهار المزيد من التقييمات
+      reviewsContainer.addEventListener("scroll", function () {
+        if (reviewsContainer.scrollTop + reviewsContainer.clientHeight >= reviewsContainer.scrollHeight) {
+          const hiddenReviews = document.querySelectorAll(".review.hidden");
+          hiddenReviews.forEach((review) => {
+            review.classList.remove("hidden");
+          });
+        }
+      });
+    }
+  }
+
+  // ✅ توليد النجوم بناءً على التقييم
+  function generateStarsHtml(stars) {
+    let fullStars = Math.floor(stars);
+    let halfStar = (stars - fullStars) >= 0.5 ? '★' : '☆';
+    let emptyStars = 5 - fullStars - (halfStar === '★' ? 1 : 0);
+
+    let starsHtml = '★'.repeat(fullStars);
+    if (halfStar === '★') starsHtml += '★';
+    starsHtml += '☆'.repeat(emptyStars);
+    
+    return starsHtml;
+  }
+});
 </script>
+
+  </script>
