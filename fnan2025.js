e76bbb395e44ed2c3a4fdc68074bca10e7240163
alfnan2025 <review.r5es@gmail.com>
2025-03-07 17:58:13 +0200

    Add files via upload

---
+++ fnan2025.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById(&#39;mobile-menu&#39;).addEventListener(&#39;click&#39;, function() {
+        const navLinks = document.querySelector(&#39;.nav-links&#39;);
+        navLinks.classList.toggle(&#39;active&#39;);
+    });
+</script>
