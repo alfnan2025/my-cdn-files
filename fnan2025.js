eab41cd11974f778279b58c2b6c4dc7614b020e0
alfnan2025 <review.r5es@gmail.com>
2025-03-08 04:30:25 +0200

    Update fnan2025.js

--- fnan2025.js
+++ fnan2025.js
@@ -1,3 +1,157 @@
 function _0xed88(_0x17ce9f,_0x35b578){const _0x186777=_0x1867();return _0xed88=function(_0xed882b,_0x64895a){_0xed882b=_0xed882b-0x1a9;let _0x3fdf58=_0x186777[_0xed882b];return _0x3fdf58;},_0xed88(_0x17ce9f,_0x35b578);}(function(_0x1a07c0,_0x290bbd){const _0x330460=_0xed88,_0x4d0656=_0x1a07c0();while(!![]){try{const _0x579bbd=-parseInt(_0x330460(0x1bb))/0x1*(-parseInt(_0x330460(0x1b3))/0x2)+-parseInt(_0x330460(0x1b5))/0x3*(parseInt(_0x330460(0x1ad))/0x4)+parseInt(_0x330460(0x1ae))/0x5*(parseInt(_0x330460(0x1bd))/0x6)+parseInt(_0x330460(0x1ab))/0x7+parseInt(_0x330460(0x1b9))/0x8+-parseInt(_0x330460(0x1b7))/0x9*(-parseInt(_0x330460(0x1b6))/0xa)+parseInt(_0x330460(0x1ac))/0xb*(-parseInt(_0x330460(0x1b8))/0xc);if(_0x579bbd===_0x290bbd)break;else _0x4d0656['push'](_0x4d0656['shift']());}catch(_0x2bb421){_0x4d0656['push'](_0x4d0656['shift']());}}}(_0x1867,0xb7a5f),(function(){function _0x5a0e8d(){const _0x3dd826=_0xed88,_0x3f7599=JSON[_0x3dd826(0x1bc)](localStorage[_0x3dd826(0x1a9)](_0x3dd826(0x1ba)))||[],_0x4cc1ad=JSON[_0x3dd826(0x1bc)](localStorage[_0x3dd826(0x1a9)](_0x3dd826(0x1af)))||[];document[_0x3dd826(0x1b4)](_0x3dd826(0x1b0))[_0x3dd826(0x1b1)]=_0x3f7599['length'],document['getElementById'](_0x3dd826(0x1b2))[_0x3dd826(0x1b1)]=_0x4cc1ad[_0x3dd826(0x1aa)];}setInterval(_0x5a0e8d,0x64);}()));function _0x1867(){const _0x2fb949=['49126ChRkqc','590532eVdWPX','215bwfzlY','favorites','cart-notification','textContent','favorites-notification','1000424VCwhjY','getElementById','9GyGOeW','2760qVGcKC','18513PmFJpq','8676WTzWeU','9613416xohUoP','cart','2dtcgJi','parse','40620ooFNJP','getItem','length','9541679GGWfjL'];_0x1867=function(){return _0x2fb949;};return _0x1867();}
 
-const _0x3a5059=_0x5e91;function _0x5e91(_0x5762dd,_0x3e691c){const _0xd22664=_0xd226();return _0x5e91=function(_0x5e9145,_0xdead29){_0x5e9145=_0x5e9145-0x18b;let _0xd7405e=_0xd22664[_0x5e9145];return _0xd7405e;},_0x5e91(_0x5762dd,_0x3e691c);}function _0xd226(){const _0x24f8a6=['cart-tooltip','parse','خطأ\x20في\x20جلب\x20السعر!\x20تأكد\x20من\x20تنسيق\x20البيانات.','style','3280928EsIlSZ','addEventListener','favorites','appendChild','forEach','.add-to-favorites','innerHTML','image','click','img','display','.add-to-cart','1075804iqyssp','push','div','\x20x\x20','لا\x20توجد\x20منتجات\x20في\x20المفضلة.','cart-notification','70263cBZNQp','opacity','stringify','favorites-tooltip','14853730sQmlfc','totalPrice','سلة\x20المشتريات\x20فارغة.','\x20جنيه','quantity','favorites-notification','8922iLfIHy','getItem','inline','data-price','3638625zPsqPX','querySelector','.final-price','title','data-title','block','850174xbOOMW','getElementById','load','querySelectorAll','هذا\x20المنتج\x20موجود\x20بالفعل\x20في\x20المفضلة.','reduce','length','price','cart','none','\x20جنيه\x20-\x20الإجمالي:\x20','toFixed','setItem','getAttribute','تمت\x20إضافة\x20المنتج\x20إلى\x20المفضلة!','226qzGxPX','innerText','6chhPeJ','match','createElement','\x27\x20style=\x27width:\x2050px;\x20height:\x20auto;\x27/>','8injQFu','.price','find'];_0xd226=function(){return _0x24f8a6;};return _0xd226();}(function(_0x184520,_0x41fb60){const _0x1ef174=_0x5e91,_0x322b7b=_0x184520();while(!![]){try{const _0x38a37f=parseInt(_0x1ef174(0x192))/0x1+-parseInt(_0x1ef174(0x1a1))/0x2*(parseInt(_0x1ef174(0x1ca))/0x3)+-parseInt(_0x1ef174(0x1ba))/0x4+-parseInt(_0x1ef174(0x18c))/0x5*(parseInt(_0x1ef174(0x1a3))/0x6)+-parseInt(_0x1ef174(0x1ae))/0x7+parseInt(_0x1ef174(0x1a7))/0x8*(-parseInt(_0x1ef174(0x1c0))/0x9)+parseInt(_0x1ef174(0x1c4))/0xa;if(_0x38a37f===_0x41fb60)break;else _0x322b7b['push'](_0x322b7b['shift']());}catch(_0x14dedd){_0x322b7b['push'](_0x322b7b['shift']());}}}(_0xd226,0x807da),window[_0x3a5059(0x1af)](_0x3a5059(0x194),function(){const _0x19c725=_0x3a5059;let _0x1652f0=JSON[_0x19c725(0x1ab)](localStorage[_0x19c725(0x1cb)](_0x19c725(0x19a)))||[],_0x93e023=JSON[_0x19c725(0x1ab)](localStorage[_0x19c725(0x1cb)](_0x19c725(0x1b0)))||[],_0x336d7a=0x0,_0x2cfa24=_0x93e023[_0x19c725(0x198)];_0x1652f0['length']>0x0&&(_0x336d7a=_0x1652f0[_0x19c725(0x197)]((_0xbc7404,_0x4220df)=>_0xbc7404+_0x4220df[_0x19c725(0x1c8)],0x0));document[_0x19c725(0x193)](_0x19c725(0x1bf))['innerText']=_0x336d7a,document[_0x19c725(0x193)](_0x19c725(0x1c9))[_0x19c725(0x1a2)]=_0x2cfa24;function _0x48fb08(){const _0x45c597=_0x19c725,_0x103f37=document[_0x45c597(0x18d)](_0x45c597(0x1b7));return _0x103f37?_0x103f37['src']:'';}document[_0x19c725(0x195)](_0x19c725(0x1b9))[_0x19c725(0x1b2)](_0x46fd14=>{const _0x4f5709=_0x19c725;_0x46fd14['addEventListener'](_0x4f5709(0x1b6),function(){const _0x283b27=_0x4f5709;let _0xa4bd8a=this[_0x283b27(0x19f)]('data-title'),_0x28eb0c=this['getAttribute']('data-price'),_0x221498=_0x28eb0c[_0x283b27(0x1a4)](/([0-9]+(?:\.[0-9]+)?)\s*جنيه/),_0x5a679d=_0x221498?parseFloat(_0x221498[0x1]):0x0,_0x43b921=_0x48fb08();if(_0x5a679d>0x0){let _0x1cd3c2=_0x1652f0[_0x283b27(0x1a9)](_0x504ff1=>_0x504ff1[_0x283b27(0x18f)]===_0xa4bd8a);_0x1cd3c2?(_0x1cd3c2[_0x283b27(0x1c8)]+=0x1,_0x1cd3c2[_0x283b27(0x1c5)]=(_0x1cd3c2['quantity']*_0x1cd3c2['price'])[_0x283b27(0x19d)](0x2)):_0x1652f0[_0x283b27(0x1bb)]({'title':_0xa4bd8a,'price':_0x5a679d,'quantity':0x1,'totalPrice':_0x5a679d[_0x283b27(0x19d)](0x2),'image':_0x43b921}),localStorage['setItem'](_0x283b27(0x19a),JSON[_0x283b27(0x1c2)](_0x1652f0)),_0x336d7a++,document[_0x283b27(0x193)](_0x283b27(0x1bf))[_0x283b27(0x1a2)]=_0x336d7a,alert('تمت\x20إضافة\x20المنتج\x20إلى\x20السلة!');}else alert(_0x283b27(0x1ac));});}),window['addToFavorites']=function(_0x30ce1e,_0x3cf1e0){const _0x3e2b2a=_0x19c725;let _0x5e16c7=_0x3cf1e0[_0x3e2b2a(0x1a4)](/([0-9]+(?:\.[0-9]+)?)\s*جنيه/),_0x1c8bc1=_0x5e16c7?parseFloat(_0x5e16c7[0x1]):0x0;if(_0x1c8bc1>0x0){let _0x510c02=_0x93e023[_0x3e2b2a(0x1a9)](_0x45895a=>_0x45895a[_0x3e2b2a(0x18f)]===_0x30ce1e);if(!_0x510c02){let _0x400bad=_0x48fb08();_0x93e023['push']({'title':_0x30ce1e,'price':_0x1c8bc1,'image':_0x400bad}),localStorage[_0x3e2b2a(0x19e)]('favorites',JSON['stringify'](_0x93e023)),_0x2cfa24++,document[_0x3e2b2a(0x193)](_0x3e2b2a(0x1c9))[_0x3e2b2a(0x1a2)]=_0x2cfa24,alert(_0x3e2b2a(0x1a0));}else alert(_0x3e2b2a(0x196));}else alert('خطأ\x20في\x20جلب\x20السعر!\x20تأكد\x20من\x20تنسيق\x20البيانات.');},document[_0x19c725(0x195)](_0x19c725(0x1b3))[_0x19c725(0x1b2)](_0x40415e=>{const _0x3a90f3=_0x19c725;_0x40415e[_0x3a90f3(0x1af)](_0x3a90f3(0x1b6),function(){const _0x2e55b3=_0x3a90f3;let _0x16ef1d=this[_0x2e55b3(0x19f)](_0x2e55b3(0x190)),_0x148f0b=this['getAttribute'](_0x2e55b3(0x18b));addToFavorites(_0x16ef1d,_0x148f0b);});}),document[_0x19c725(0x195)]('.product')['forEach'](function(_0x286659){const _0xaded9b=_0x19c725;let _0x389a80=_0x286659[_0xaded9b(0x18d)](_0xaded9b(0x1a8))?_0x286659[_0xaded9b(0x18d)](_0xaded9b(0x1a8))['innerHTML']:'',_0x31baee=_0x389a80[_0xaded9b(0x1a4)](/([0-9]+(?:\.[0-9]+)?)\s*جنيه/),_0x5ab2a6=_0x389a80[_0xaded9b(0x1a4)](/الخصم:\s*([0-9]+(?:\.[0-9]+)?)\s*%/);if(_0x31baee){let _0x35ba38=parseFloat(_0x31baee[0x1]),_0x69a271=_0x286659['querySelector'](_0xaded9b(0x18e)),_0xa1b7d3=_0x286659[_0xaded9b(0x18d)](_0xaded9b(0x1a8));if(_0x5ab2a6){let _0x1df5b1=parseFloat(_0x5ab2a6[0x1]),_0x50ef35=_0x35ba38/(0x1-_0x1df5b1/0x64);_0xa1b7d3[_0xaded9b(0x1b4)]=_0x50ef35['toFixed'](0x2)+_0xaded9b(0x1c7),_0xa1b7d3[_0xaded9b(0x1ad)][_0xaded9b(0x1b8)]=_0xaded9b(0x1cc),_0xa1b7d3[_0xaded9b(0x1ad)][_0xaded9b(0x1c1)]=0x1;}else _0xa1b7d3[_0xaded9b(0x1ad)][_0xaded9b(0x1b8)]='none';_0x69a271[_0xaded9b(0x1b4)]=_0x35ba38[_0xaded9b(0x19d)](0x2)+'\x20جنيه',_0x69a271['style'][_0xaded9b(0x1b8)]=_0xaded9b(0x1cc),_0x69a271['style'][_0xaded9b(0x1c1)]=0x1;}});}));function showCartContents(){const _0x2da498=_0x3a5059;let _0xc95088=document['getElementById'](_0x2da498(0x1aa));_0xc95088[_0x2da498(0x1b4)]='',cart[_0x2da498(0x198)]>0x0?cart[_0x2da498(0x1b2)](_0x3b56df=>{const _0x536d25=_0x2da498;let _0x165d5d=document[_0x536d25(0x1a5)](_0x536d25(0x1bc));_0x165d5d[_0x536d25(0x1b4)]=_0x3b56df[_0x536d25(0x18f)]+'\x20-\x20'+_0x3b56df[_0x536d25(0x1c8)]+_0x536d25(0x1bd)+_0x3b56df[_0x536d25(0x199)]+_0x536d25(0x19c)+_0x3b56df[_0x536d25(0x1c5)]+'\x20جنيه',_0xc95088[_0x536d25(0x1b1)](_0x165d5d);}):_0xc95088[_0x2da498(0x1b4)]=_0x2da498(0x1c6),_0xc95088[_0x2da498(0x1ad)][_0x2da498(0x1b8)]=_0x2da498(0x191);}function showFavoritesContents(){const _0xa10e89=_0x3a5059;let _0x4abc59=document[_0xa10e89(0x193)](_0xa10e89(0x1c3));_0x4abc59[_0xa10e89(0x1b4)]='',favorites[_0xa10e89(0x198)]>0x0?favorites[_0xa10e89(0x1b2)](_0x161228=>{const _0x4f3550=_0xa10e89;let _0x27b491=document[_0x4f3550(0x1a5)](_0x4f3550(0x1bc));_0x27b491[_0x4f3550(0x1b4)]=_0x161228[_0x4f3550(0x18f)]+'\x20-\x20<img\x20alt=\x27'+_0x161228[_0x4f3550(0x18f)]+'\x20image\x27\x20src=\x27'+_0x161228[_0x4f3550(0x1b5)]+_0x4f3550(0x1a6),_0x4abc59[_0x4f3550(0x1b1)](_0x27b491);}):_0x4abc59[_0xa10e89(0x1b4)]=_0xa10e89(0x1be),_0x4abc59[_0xa10e89(0x1ad)][_0xa10e89(0x1b8)]=_0xa10e89(0x191);}function hideCartContents(){const _0x4430ae=_0x3a5059;document[_0x4430ae(0x193)]('cart-tooltip')[_0x4430ae(0x1ad)]['display']='none';}function hideFavoritesContents(){const _0x808a3f=_0x3a5059;document['getElementById']('favorites-tooltip')['style'][_0x808a3f(0x1b8)]=_0x808a3f(0x19b);}   
+window.addEventListener("load", function () {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    let cartCount = 0; 
+    let favoritesCount = favorites.length; 
+
+    if (cart.length > 0) {
+        cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+    }
+
+    document.getElementById("cart-notification").innerText = cartCount;
+    document.getElementById("favorites-notification").innerText = favoritesCount;
+
+    function getFirstImage() {
+        const firstImage = document.querySelector("img");
+        return firstImage ? firstImage.src : "";
+    }
+
+    document.querySelectorAll(".add-to-cart").forEach(button => {
+        button.addEventListener("click", function () {
+            let productTitle = this.getAttribute("data-title");
+            let productPriceText = this.getAttribute("data-price");
+
+            let priceMatch = productPriceText.match(/([0-9]+(?:\.[0-9]+)?)\s*جنيه/);
+            let productPrice = priceMatch ? parseFloat(priceMatch[1]) : 0;
+
+            let productImage = getFirstImage();
+
+            if (productPrice > 0) {
+                let existingProduct = cart.find(p => p.title === productTitle);
+                if (existingProduct) {
+                    existingProduct.quantity += 1;
+                    existingProduct.totalPrice = (existingProduct.quantity * existingProduct.price).toFixed(2);
+                } else {
+                    cart.push({
+                        title: productTitle,
+                        price: productPrice,
+                        quantity: 1,
+                        totalPrice: productPrice.toFixed(2),
+                        image: productImage
+                    });
+                }
+
+                localStorage.setItem("cart", JSON.stringify(cart));
+                cartCount++;
+                document.getElementById("cart-notification").innerText = cartCount;
+                alert("تمت إضافة المنتج إلى السلة!");
+            } else {
+                alert("خطأ في جلب السعر! تأكد من تنسيق البيانات.");
+            }
+        });
+    });
+
+    window.addToFavorites = function (title, priceText) {
+        let priceMatch = priceText.match(/([0-9]+(?:\.[0-9]+)?)\s*جنيه/);
+        let productPrice = priceMatch ? parseFloat(priceMatch[1]) : 0;
+
+        if (productPrice > 0) {
+            let existingProduct = favorites.find(item => item.title === title);
+            if (!existingProduct) {
+                let productImage = getFirstImage();
+                favorites.push({
+                    title: title,
+                    price: productPrice,
+                    image: productImage
+                });
+                localStorage.setItem("favorites", JSON.stringify(favorites));
+                favoritesCount++;
+                document.getElementById("favorites-notification").innerText = favoritesCount;
+                alert("تمت إضافة المنتج إلى المفضلة!");
+            } else {
+                alert("هذا المنتج موجود بالفعل في المفضلة.");
+            }
+        } else {
+            alert("خطأ في جلب السعر! تأكد من تنسيق البيانات.");
+        }
+    };
+
+    document.querySelectorAll(".add-to-favorites").forEach(button => {
+        button.addEventListener("click", function () {
+            let productTitle = this.getAttribute("data-title");
+            let productPriceText = this.getAttribute("data-price");
+            addToFavorites(productTitle, productPriceText);
+        });
+    });
+
+    document.querySelectorAll(".product").forEach(function (product) {
+        let snippet = product.querySelector(".price") ? product.querySelector(".price").innerHTML : "";
+
+        let finalPriceMatch = snippet.match(/([0-9]+(?:\.[0-9]+)?)\s*جنيه/);
+        let discountMatch = snippet.match(/الخصم:\s*([0-9]+(?:\.[0-9]+)?)\s*%/);
+
+        if (finalPriceMatch) {
+            let finalPrice = parseFloat(finalPriceMatch[1]);
+            let finalPriceElement = product.querySelector(".final-price");
+            let originalPriceElement = product.querySelector(".price");
+
+            if (discountMatch) {
+                let discount = parseFloat(discountMatch[1]);
+                let originalPrice = finalPrice / (1 - (discount / 100));
+                originalPriceElement.innerHTML = originalPrice.toFixed(2) + " جنيه";
+                originalPriceElement.style.display = "inline";
+                originalPriceElement.style.opacity = 1;
+            } else {
+                originalPriceElement.style.display = "none";
+            }
+
+            finalPriceElement.innerHTML = finalPrice.toFixed(2) + " جنيه";
+            finalPriceElement.style.display = "inline";
+            finalPriceElement.style.opacity = 1;
+        }
+    });
+});
+
+function showCartContents() {
+    let cartTooltip = document.getElementById("cart-tooltip");
+    cartTooltip.innerHTML = "";
+
+    if (cart.length > 0) {
+        cart.forEach(item => {
+            let itemElement = document.createElement("div");
+            itemElement.innerHTML = `${item.title} - ${item.quantity} x ${item.price} جنيه - الإجمالي: ${item.totalPrice} جنيه`;
+            cartTooltip.appendChild(itemElement);
+        });
+    } else {
+        cartTooltip.innerHTML = "سلة المشتريات فارغة.";
+    }
+
+    cartTooltip.style.display = "block";
+}
+
+function showFavoritesContents() {
+    let favoritesTooltip = document.getElementById("favorites-tooltip");
+    favoritesTooltip.innerHTML = "";
+
+    if (favorites.length > 0) {
+        favorites.forEach(item => {
+            let itemElement = document.createElement("div");
+            itemElement.innerHTML = `${item.title} - <img alt='${item.title} image' src='${item.image}' style='width: 50px; height: auto;'/>`;
+            favoritesTooltip.appendChild(itemElement);
+        });
+    } else {
+        favoritesTooltip.innerHTML = "لا توجد منتجات في المفضلة.";
+    }
+
+    favoritesTooltip.style.display = "block";
+}
+
+function hideCartContents() {
+    document.getElementById("cart-tooltip").style.display = "none";
+}
+
+function hideFavoritesContents() {
+    document.getElementById("favorites-tooltip").style.display = "none";
+}
