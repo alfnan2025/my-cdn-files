5156d6ef7ba731aed27043e07c3bbcdca18dc0a4
alfnan2025 <review.r5es@gmail.com>
2025-03-15 00:23:23 +0200

    Delete tdres.home.posts.js

--- tdres.home.posts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.location.href.includes("?q=")) { 
-        var categorySidebar = document.querySelector(".category-sidebar");
-        
-        if (categorySidebar) {
-            categorySidebar.style.display = "none";
-        }
-    }
-});
